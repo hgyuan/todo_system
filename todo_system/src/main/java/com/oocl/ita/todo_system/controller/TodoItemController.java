3d9a76f6c6b3d762b72ba2d800fc28b0661b4c6b
1764775976@qq.com <1764775976@qq.com>
2020-08-06 21:19:13 +0800

    [todo_system][max,richard]feature: add controller

--- todo_system/src/main/java/com/oocl/ita/todo_system/controller/TodoItemController.java
+++ todo_system/src/main/java/com/oocl/ita/todo_system/controller/TodoItemController.java
@@ -1,10 +1,10 @@
 package com.oocl.ita.todo_system.controller;
 
+import com.oocl.ita.todo_system.dto.TodoItemRequest;
 import com.oocl.ita.todo_system.dto.TodoItemResponse;
 import com.oocl.ita.todo_system.service.TodoItemService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.sun.org.apache.regexp.internal.RE;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,9 +19,23 @@ public class TodoItemController {
     }
 
     @GetMapping()
-    public List<TodoItemResponse> getTodoItems(){
+    public List<TodoItemResponse> getTodoItems() {
         return todoItemService.getTodoItems();
     }
 
+    @PostMapping()
+    public TodoItemResponse addTodoItem(@RequestBody TodoItemRequest todoItemRequest) {
+        return todoItemService.addTodoItem(todoItemRequest);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTodoItemById(@PathVariable Integer id) {
+        todoItemService.deleteTodoItemById(id);
+    }
+
+    @PutMapping("/{id}")
+    public TodoItemResponse updateTodoItemById(@PathVariable Integer id, @RequestBody TodoItemRequest todoItemRequest) {
+        return todoItemService.updateTodoItem(todoItemRequest, id);
+    }
 
 }
--- todo_system/src/main/java/com/oocl/ita/todo_system/service/TodoItemService.java
+++ todo_system/src/main/java/com/oocl/ita/todo_system/service/TodoItemService.java
@@ -2,13 +2,15 @@ package com.oocl.ita.todo_system.service;
 
 import com.oocl.ita.todo_system.dto.TodoItemRequest;
 import com.oocl.ita.todo_system.dto.TodoItemResponse;
-import com.oocl.ita.todo_system.entity.TodoItem;
 
 import java.util.List;
 
 public interface TodoItemService {
     List<TodoItemResponse> getTodoItems();
+
     void deleteTodoItemById(Integer id);
+
     TodoItemResponse addTodoItem(TodoItemRequest todoItemRequest);
+
     TodoItemResponse updateTodoItem(TodoItemRequest todoItemRequest, Integer id);
 }
