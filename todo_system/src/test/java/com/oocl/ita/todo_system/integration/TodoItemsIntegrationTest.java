48d40e9009acd0af60bca67608b9709261fab1e6
1764775976@qq.com <1764775976@qq.com>
2020-08-06 21:35:09 +0800

    [todo_system][max,richard]test: should_return_todo_item_response_when_add_todo_item_given_1_todo_item_request

--- todo_system/src/test/java/com/oocl/ita/todo_system/integration/TodoItemsIntegrationTest.java
+++ todo_system/src/test/java/com/oocl/ita/todo_system/integration/TodoItemsIntegrationTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -39,4 +40,16 @@ public class TodoItemsIntegrationTest {
                 .andExpect(jsonPath("[0].text").value("123"));
     }
 
+    @Test
+    void should_return_todo_item_response_when_add_todo_item_given_1_todo_item_request() throws Exception {
+
+        String value = "{\n" +
+                "\n" +
+                "    \"text\":\"123123\",\n" +
+                "    \"mark\":true\n" +
+                "\n" +
+                "}";
+        mockMvc.perform(post("/todoItems").contentType(MediaType.APPLICATION_JSON).content(value))
+                .andExpect(jsonPath("text").value("123123"));
+    }
 }
